f11cb7cce5ff030eab95e0beb425375255edfeea
Tom Glenn <tomglenn91@gmail.com>
2022-01-16 22:57:41 +0000

    remove types

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import { Berry, BerrySelect } from "./BerrySelect";
+import { BerrySelect } from "./BerrySelect";
 import { PokemonCard, CapturedPokemonCard } from "./PokemonCard";
 import { usePokemon } from "./use-pokemon";
 
 function App() {
   const pokemon = usePokemon();
-  const [berry, setBerry] = useState<Berry>("Berry");
+  const [berry, setBerry] = useState("Berry");
 
   return (
     <div>
       <div>
-        <BerrySelect selectedBerry={berry} onBerrySelected={(b) => setBerry(b)}></BerrySelect>
+        <BerrySelect selectedBerry={berry} onBerrySelected={(b: any) => setBerry(b)}></BerrySelect>
       </div>
       {pokemon.map((p) => {
         const CardComponent = p.name[0] === "c" ? CapturedPokemonCard : PokemonCard;
--- src/BerrySelect.tsx
+++ src/BerrySelect.tsx
@@ -1,13 +1,7 @@
-import { ChangeEventHandler, FC, ReactEventHandler, useCallback } from "react";
+import { FC, useCallback } from "react";
 import styled from "styled-components";
 
-const berries = ["Berry", "Gold Berry", "Mint Berry"] as const;
-export type Berry = typeof berries[number];
-
-export type BerrySelectProps = {
-  selectedBerry: Berry;
-  onBerrySelected: (berry: Berry) => void;
-};
+const berries = ["Berry", "Gold Berry", "Mint Berry"];
 
 const BerryTitle = styled.h1`
   margin-left: 8px;
@@ -20,9 +14,9 @@ const BerrySelecter = styled.select`
   width: 300px;
 `;
 
-export const BerrySelect: FC<BerrySelectProps> = ({ onBerrySelected, selectedBerry }) => {
-  const handleBerrySelected = useCallback<ChangeEventHandler<HTMLSelectElement>>((e) => {
-    onBerrySelected(e.currentTarget.value as Berry);
+export const BerrySelect: FC<any> = ({ onBerrySelected, selectedBerry }) => {
+  const handleBerrySelected = useCallback((e: any) => {
+    onBerrySelected(e.currentTarget.value);
   }, []);
 
   return (
